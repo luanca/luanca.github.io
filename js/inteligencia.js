b67228b2059840b1a283d89659e622556dbba73f
Luan Carvalho <luanc.jw@gmail.com>
2023-01-08 00:53:25 -0300

    8.1.2023

--- js/inteligencia.js
+++ js/inteligencia.js
@@ -32,7 +32,7 @@ function idade(nascimento) {
 
 function preencher() {
   // função para preencher as informações na página
-  var vp1, vp2, vp4, vp5, vp6;
+  var vp1, vp2, vp4, vp5, vp6, vp8;
   vp1 = new XMLHttpRequest();
   vp1.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
@@ -43,19 +43,25 @@ function preencher() {
       document.getElementById("fotoPerfil").src = vp2.FotoPerfil;
       document.getElementById("idade").innerHTML = "Idade: " + idade(vp2.DataNascimento.ano + "-" + vp2.DataNascimento.mes + "-" + vp2.DataNascimento.dia);
       document.getElementById("resideEm").innerText = vp2.ResideEm;
-      vp2.Links.forEach(vp3 => {
-        vp4 = document.createElement("img");
-        vp4.setAttribute("src", vp3.Icone);
-        vp4.setAttribute("alt", "Ícone de " + vp3.Nome);
-        vp5 = document.createElement("span");
-        vp5.innerText = vp3.Nome;
-        vp6 = document.createElement("a");
-        vp6.setAttribute("href", vp3.Url);
-        vp6.setAttribute("target", "_blank");
-        vp6.setAttribute("class", "btnLink");
-        vp6.appendChild(vp4);
-        vp6.appendChild(vp5);
-        document.getElementById("links").appendChild(vp6);
+      vp2.Links.forEach(vp7 => {
+        vp8 = document.createElement("h3");
+        vp8.innerText = vp7.Categoria;
+        vp8.setAttribute("class", "catLinks");
+        document.getElementById("links").appendChild(vp8);
+        vp7.Links.forEach(vp3 => {
+          vp4 = document.createElement("img");
+          vp4.setAttribute("src", vp3.Icone);
+          vp4.setAttribute("alt", "Ícone de " + vp3.Nome);
+          vp5 = document.createElement("span");
+          vp5.innerText = vp3.Nome;
+          vp6 = document.createElement("a");
+          vp6.setAttribute("href", vp3.Url);
+          vp6.setAttribute("target", "_blank");
+          vp6.setAttribute("class", "btnLink");
+          if(vp3.Icone !== null){vp6.appendChild(vp4)};
+          vp6.appendChild(vp5);
+          document.getElementById("links").appendChild(vp6);
+        });
       });
     }
   };
